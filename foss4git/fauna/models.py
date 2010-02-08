05f23ebcd72c35a9fdc535626155bbbb88e7bf0c
geodjango <geodjango@geodjango-laptop.(none)>
2010-02-08 17:54:23 +0100

    Potato il modello (tolto anche la parte GIS per il primo pezzo del tutorial

--- foss4git/fauna/models.py
+++ foss4git/fauna/models.py
@@ -1,41 +1,23 @@
 from django.db import models
-from django.contrib.gis.db import models as gismodels
 
-# modelli
 class Animale(models.Model):
     """Modello per rappresentare gli animali"""
     nome = models.CharField(max_length=50, unique = True)
-    foto = models.ImageField(upload_to='animali.foto')
+    foto = models.ImageField(upload_to='animali.foto',blank=True, null=True)
 
     def __unicode__(self):
         return '%s' % (self.nome)
-
-    def image_url(self):
-        print '***%s****' % self.foto.url
-        return u'<img src="%s" alt="%s" width="80"></img>' % (self.foto.url, self.nome)
-    image_url.short_description = "Foto"
-    image_url.allow_tags = True
 
     class Meta:
         ordering = ['nome']
         verbose_name_plural = "Animali"
 
-class Avvistamento(gismodels.Model):
+class Avvistamento(models.Model):
     """Modello spaziale per rappresentare gli avvistamenti"""
 
-    LIVELLI_INTERESSE = (
-        (1, '*'),
-        (2, '**'),
-        (3, '***'),
-        (4, '****'),
-        (5, '*****'),
-    )
-    data = gismodels.DateTimeField()
-    note = gismodels.TextField()
-    interesse = gismodels.IntegerField(choices=LIVELLI_INTERESSE)
-    animale = gismodels.ForeignKey(Animale)
-    geometry = gismodels.PointField(srid=4326)
-    objects = gismodels.GeoManager()
+    data = models.DateTimeField()
+    note = models.TextField(blank=True, null=True)
+    animale = models.ForeignKey(Animale)
 
     def __unicode__(self):
         return '%s' % (self.data)
@@ -43,35 +25,4 @@ class Avvistamento(gismodels.Model):
     class Meta:
         ordering = ['data']
         verbose_name_plural = "Avvistamenti"
-
-class Regione(gismodels.Model):
-    """Modello spaziale per rappresentare le regioni"""
-    codice = gismodels.IntegerField()
-    nome = gismodels.CharField(max_length=50)
-    geometry = gismodels.MultiPolygonField(srid=4326)
-    objects = gismodels.GeoManager()
-
-    def __unicode__(self):
-        return '%s' % (self.nome)
-
-class Provincia(gismodels.Model):
-    """Modello spaziale per rappresentare le regioni"""
-    codice = gismodels.IntegerField()
-    nome = gismodels.CharField(max_length=50)
-    geometry = gismodels.MultiPolygonField(srid=4326)
-    objects = gismodels.GeoManager()
-
-    def __unicode__(self):
-        return '%s' % (self.nome)
-
-class SandboxLayer(gismodels.Model):
-    """Modello spaziale per effettuare test con l'API GeoDjango"""
-    nome = gismodels.CharField(max_length=50)
-    geometry = gismodels.GeometryField(srid=3395) # WGS84 mercatore
-    objects = gismodels.GeoManager()
-
-    def __unicode__(self):
-        return '%s' % (self.nome)
-
-
 
--- foss4git/settings.py
+++ foss4git/settings.py
@@ -18,10 +18,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tutorial',                      # Or path to database file if using sqlite3.
-        'USER': 'ale',                      # Not used with sqlite3.
-        'PASSWORD': 'ale',                  # Not used with sqlite3.
+        'USER': 'geodjango',                      # Not used with sqlite3.
+        'PASSWORD': 'geodjango',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -91,4 +91,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.gis',
     'foss4git.fauna',
+    'foss4git.ciaomondo',
+
 )
